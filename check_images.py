927245e154684f0d3db3197902a4ef75c4ced064
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-11-19 17:23:31 +0200

    Initial commit

---
+++ check_images.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/check_images.py
+#                                                                          
+# PROGRAMMER: Neven Dyulgerov
+# DATE CREATED: 15/11/2018                    
+# REVISED DATE:
+# PURPOSE: Classifies pet images using a pretrained CNN model, compares these
+#          classifications to the true identity of the pets in the images, and
+#          summarizes how well the CNN performed on the image classification task. 
+#          Note that the true identity of the pet (or object) in the image is 
+#          indicated by the filename of the image. Therefore, your program must
+#          first extract the pet image label from the filename before
+#          classifying the images using the pretrained CNN model. With this 
+#          program we will be comparing the performance of 3 different CNN model
+#          architectures to determine which provides the 'best' classification.
+#
+# Use argparse Expected Call with <> indicating expected user input:
+#      python check_images.py --dir <directory with images> --arch <model>
+#             --dogfile <file that contains dognames>
+#   Example call:
+#    python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt
+##
+
+# Imports python modules
+from time import time, sleep
+
+# Imports print functions that check the lab
+from print_functions_for_lab_checks import *
+
+# Imports functions created for this program
+from get_input_args import get_input_args
+from get_pet_labels import get_pet_labels
+from classify_images import classify_images
+from adjust_results4_isadog import adjust_results4_isadog
+from calculates_results_stats import calculates_results_stats
+from print_results import print_results
+
+# Main program function defined below
+def main():
+    # log start time
+    start_time = time()
+    
+    # get input
+    in_arg = get_input_args()
+    
+    # check input
+    check_command_line_arguments(in_arg)
+    
+    # get pet labels
+    results = get_pet_labels(in_arg.dir)
+    
+    # check pet labels
+    check_creating_pet_image_labels(results)
+    
+    # classify images
+    classify_images(in_arg.dir, results, in_arg.arch)
+
+    # check image classification   
+    check_classifying_images(results)    
+
+    # adjust results for dogs
+    adjust_results4_isadog(results, in_arg.dogfile)
+
+    # check adjusted results for dogs
+    check_classifying_labels_as_dogs(results)
+
+    # get results stats
+    results_stats = calculates_results_stats(results)
+
+    # check results stats
+    check_calculating_results(results, results_stats)
+
+
+    # TODO 6: Define print_results function within the file print_results.py
+    # Once the print_results function has been defined replace 'None' 
+    # in the function call with in_arg.arch  Once you have done the 
+    # replacements your function call should look like this: 
+    #      print_results(results, results_stats, in_arg.arch, True, True)
+    # Prints summary results, incorrect classifications of dogs (if requested)
+    # and incorrectly classified breeds (if requested)
+    
+    print_results(results, results_stats, in_arg.arch, True, True)
+    
+    end_time = time()
+    
+    tot_time = end_time - start_time
+    print("\n** Total Elapsed Runtime:",
+          str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
+          +str(int((tot_time%3600)%60)) )
+    
+
+# Call to main function to run the program
+if __name__ == "__main__":
+    main()
